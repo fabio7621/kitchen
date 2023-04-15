39c024c3f72b448d3676df15411cc367e65e8ebd
fabio7621 <d6200121a@gmail.com>
2023-04-15 14:42:18 +0800

    1

--- run.js
+++ run.js
@@ -16,3 +16,12 @@ function startInterval() {
 	count = 500;
 	interval = setInterval(updateNumber, 20);
 }
+document.getElementById("hb1").addEventListener("click", function () {
+	document.getElementById("m2").style.display = "block";
+	/* 點擊hb1時，將m2區塊的display屬性設定為block，顯示出來 */
+});
+
+document.getElementById("hb2").addEventListener("click", function () {
+	document.getElementById("m2").style.display = "none";
+	/* 點擊hb2時，將m2區塊的display屬性設定為none，隱藏起來 */
+});
